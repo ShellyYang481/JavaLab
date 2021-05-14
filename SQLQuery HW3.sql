1ca3daaee16ebde018a8b88b61e112dc7f808d44
ShellyYang481 <83637032+ShellyYang481@users.noreply.github.com>
2021-05-14 18:20:46 +0800

    Add files via upload

---
+++ SQLQuery HW3.sql
@@ -0,0 +1,21 @@
+1.
+select getdate() as 系統日期;
+2.
+select empno, ename, sal, convert(int,sal*1.15 ) "New Salary" from emp
+3.
+select empno, ename, sal, convert(int,sal*1.15 ) 'New Salary',sal*0.15 'Increase' from emp
+4. 
+select ename, datediff(month,hiredate,getdate()) as Months_Worked from emp;
+5.
+select concat (ename,' earns ',sal,' monthly but wants ' ,  sal*3)'Dream Salaries' from emp;
+7.
+select ename,(sal, 16) from emp
+8.
+select ename, comm, iif(comm is null,'No Commision',comm)
+from emp
+9.
+select ename,
+replicate('*', sal/100) 'EMPLOYEE_AND_THEIR_SALARIES'
+from emp
+
+	
